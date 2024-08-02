bc0a4ed94e87e4d96c05dfcc2f03d0488dd0f2ea
Suppe <Suppe04@gmail.com>
2024-08-02 15:55:45 +0200

    Updated CMS functions

--- src/Supabase/cms.ts
+++ src/Supabase/cms.ts
@@ -1,8 +1,9 @@
 import {supabase} from "./SupabaseClient"
 
-export async function signInWithEmail(userEmail: string, userPassword: string) {
+export const signInWithEmail = async (userEmail: string, userPassword: string): Promise<any> => {
     const { data, error } = await supabase.auth.signInWithPassword({
       email: userEmail,
       password: userPassword,
     })
-  }
+    return data;
+}
